bc98b3007d6c29b3537eecc7bfd79720b6b2f679
Amal Hussein <amalhussein08@gmail.com>
2013-11-21 11:55:59 -0500

    added in changes for resque setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,23 @@ class HomeController < ApplicationController
 	def show
 	end
 
-	# POST to send_email
-	def send_email
-		email_to = params["email_destination"]
-		email_subject = params["email_subject"]
-		email_body = params["email_body"]
-		email_from = current_user.email
-		Mail.deliver do
-  		to email_to
-  		from email_from
-  		subject email_subject
-  		body email_body
-		end
+
+	def send_email 
+		Resque.enqueue(SendBackgroundEmail, current_user.email, params)
 		redirect_to root_url, notice: "Email sent"
-	end
+	end 
+	# # POST to send_email
+	# def send_email
+	# 	email_to = params["email_destination"]
+	# 	email_subject = params["email_subject"]
+	# 	email_body = params["email_body"]
+	# 	email_from = current_user.email
+	# 	Mail.deliver do
+ #  		to email_to
+ #  		from email_from
+ #  		subject email_subject
+ #  		body email_body
+	# 	end
+	# 	redirect_to root_url, notice: "Email sent"
+	# end
 end
