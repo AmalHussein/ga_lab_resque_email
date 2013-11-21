00686c3b9d9b3bf4a00892dc6df2aaf07dc7e88c
Amal Hussein <amalhussein08@gmail.com>
2013-11-21 11:00:44 -0500

    commit before pull

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
-	def show
-	end
+
+def show
+end
+  
+  #POST to send_email
+  def send_email
+    email_to = params['email_destination']
+    email_from = current_user.email
+    email_subject = params['email_subject']
+    email_body = params['email_body']
+
+    Mail.deliver do
+      to email_to
+      from email_from
+      subject email_subject
+      body email_body
+    end
+    redirect_to root_url, notice: "Email sent"
+  end
+
 end
